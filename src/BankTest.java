3f05ac4f5f951fdf7ea64593292d6e30484d5a01
uglynora <mangos8@yahoo.com>
2019-04-14 14:16:14 -0400

    Testing Bank

---
+++ src/BankTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+public class BankTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void addAccountWizard() {
+    }
+
+    @org.junit.Test
+    public void addCustomerWizard() {
+    }
+
+    @org.junit.Test
+    public void addCustomer() {
+    }
+
+    @org.junit.Test
+    public void removeCustomer() {
+    }
+}
