efb8e2a09933964d76276d44621026b3ef0cfa7c
uglynora <mangos8@yahoo.com>
2019-04-14 17:45:04 -0400

    More testing than before.

---
+++ tests/AccountTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void transfer() {
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+    }
+
+    @org.junit.Test
+    public void getAccountId() {
+    }
+
+    @org.junit.Test
+    public void getCustomerId() {
+    }
+
+    @org.junit.Test
+    public void hashCode() {
+    }
+
+    @org.junit.Test
+    public void equals() {
+    }
+}
---
+++ tests/BankTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+public class BankTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void getNAME() {
+    }
+
+    @org.junit.Test
+    public void getAllAccounts() {
+    }
+
+    @org.junit.Test
+    public void addCustomer() {
+    }
+
+    @org.junit.Test
+    public void removeCustomer() {
+    }
+
+    @org.junit.Test
+    public void getCustomer() {
+    }
+
+    @org.junit.Test
+    public void getCustomersAccounts() {
+    }
+}
